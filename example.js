8c1da7eccbda47a50877568c00c2891bc4f19d5a
Paul.py <54999877+Paul-py07@users.noreply.github.com>
2020-02-17 14:03:13 -0600

    Initial commit

---
+++ example.js
@@ -0,0 +1,27 @@
+// var x = 75;
+// if (x > 100) {
+//     console.log("bigger than 100");
+// }
+// else if (x > 50) {
+//     console.log("bigger than 60");
+// }
+// else if(x > 25) {
+//     console.log("bigger than 25");
+// }
+// else {
+//     console.log("small number");
+// // }
+ var passPhrase = "Hello World";
+ if(passPhrase == "Open Sesame") {
+     console.log("Wrong Secret Passphrase");
+}
+else if(passPhrase == "Hello World") {
+     console.log("Success!!!")
+}
+ else {
+     console.log("Not even close")
+}
+
+num1 = 1
+
+console.log (num1)
